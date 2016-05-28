b800ceb75c3c088faec1b6e452dacf0b0993e546
lkurcius <lkurcius@us.edu.pl>
2016-05-28 16:25:39 +0200

    - robudowa pliku w celu testowania kodu

--- CodeOptymalizator/test1.java
+++ CodeOptymalizator/test1.java
@@ -4,3 +4,12 @@ int a;
 if (zmienna.equals("")){
 	a = 10;
 }
+
+int b = Math.abs(10);
+
+StringBuffer buf = new StringBuffer();
+buf.append("Hello").append(" ").append("World");
+
+for(int i = 0; i < 10; i++){
+        int a;
+        }
